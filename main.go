1b01ff12ee841e0279ab12063e8452bde5378502
Gildas Le Nadan <3ntr0p13+github@gmail.com>
2016-01-05 13:00:59 +0000

    initial import and adaptation of the docker-volume-garbage-generator example plugin - retrieved from github.com/calavera/docker-volume-garbage-generator - adapted to the new docker plugin helpers (github.com/docker/go-plugins-helpers) - re-branded some parts ;)

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"github.com/docker/go-plugins-helpers/volume"
+	"path/filepath"
+)
+
+const (
+	pluginName = "ebs"
+)
+
+var (
+	root = flag.String("root", volume.DefaultDockerRootDirectory, "Docker volumes root directory")
+)
+
+type ebsDriver struct {
+	root string
+}
+
+func (e ebsDriver) Create(r volume.Request) volume.Response {
+	fmt.Printf("Create %v\n", r)
+
+	return volume.Response{}
+}
+
+func (e ebsDriver) Remove(r volume.Request) volume.Response {
+	fmt.Printf("Remove %v\n", r)
+
+	return volume.Response{}
+}
+
+func (e ebsDriver) Path(r volume.Request) volume.Response {
+	fmt.Printf("Path %v\n", r)
+
+	return volume.Response{Mountpoint: filepath.Join(e.root, r.Name)}
+}
+
+func (e ebsDriver) Mount(r volume.Request) volume.Response {
+	p := filepath.Join(e.root, r.Name)
+	fmt.Printf("Mount %s\n", p)
+
+	return volume.Response{Mountpoint: p}
+}
+
+func (e ebsDriver) Unmount(r volume.Request) volume.Response {
+	p := filepath.Join(e.root, r.Name)
+	fmt.Printf("Unmount %s\n", p)
+
+	return volume.Response{}
+}
+
+func main() {
+	d := ebsDriver{*root}
+	h := volume.NewHandler(d)
+	h.ServeUnix("root", pluginName)
+	fmt.Printf("Starting listening on unix socket with name %s (usually in /run/docker/plugins/%s)", pluginName, pluginName)
+}
